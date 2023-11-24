051869d1f6031dded86d02e976badd0fc5b6e080
Brandon Olynyk <brandontab2004@gmail.com>
2023-11-24 12:42:47 -0500

    Images display (in very low quality?)

--- views/FeedView.js
+++ views/FeedView.js
@@ -1,8 +1,13 @@
-import {Text, Appbar} from 'react-native-paper';
-import {View} from 'react-native';
-
-function Article(){
-
+import {Button,Text, Appbar, List, Portal, Modal} from 'react-native-paper';
+import {View, ScrollView} from 'react-native';
+import {useState} from 'react';
+import { WebView } from 'react-native-webview';
+function ArticleItem({item, setter}){
+  return(<>
+  <List.Item key={item.title} title={item.title}
+  onPress={() => {setter(item)}}></List.Item>
+  
+  </>);
 }
 
 function Titlebar({title, setter}){
@@ -15,10 +20,39 @@ function Titlebar({title, setter}){
 }
 
 export function ArticleList({feed, setter}){
+
+  const [currentArticle, setCurrentArticle] = useState(null);
+  const itemJSX = feed.item.map(elem => {
+    //return <Text>{JSON.stringify(elem)}</Text>
+    return <ArticleItem item={elem} setter={setCurrentArticle}></ArticleItem>
+  })
+  
   return(
     <View>
       <Titlebar title={feed.title} setter={setter}></Titlebar>
-      <Text>Insert articles for {feed.title} here.</Text>
+      <ScrollView>
+        {currentArticle === null && (itemJSX)}
+        {currentArticle !== null && (<ArticleWebView article={currentArticle}
+        setter={setCurrentArticle}></ArticleWebView>)}
+      </ScrollView>
       </View>
   );
+  
 }
+
+function ArticleWebView({article, setter}){
+  const htmlString = article.description
+  console.log(htmlString)
+  return(
+  <Portal>
+      <Titlebar title={article.title} setter={() => {setter(null)}}></Titlebar>  
+      <WebView
+        originWhitelist={['*']}
+        source={{ html: htmlString}}>
+      </WebView>
+    
+    
+    </Portal>
+  );
+}
+
