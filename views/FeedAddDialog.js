f10de73f26a20a9f2ab52cb27de1653cf45d69c1
Brandon Olynyk <brandontab2004@gmail.com>
2023-12-01 10:16:23 -0500

    add error handling to feed picker

--- views/FeedAddDialog.js
+++ views/FeedAddDialog.js
@@ -1,4 +1,4 @@
-import {  Button,  Dialog, Portal, TextInput, ActivityIndicator } from "react-native-paper";
+import {  Button,  Dialog, Portal, TextInput, ActivityIndicator, HelperText } from "react-native-paper";
 import { fetchFeed } from '../modules/FeedFetcher'
 import {useEffect, useState} from 'react'
 import { StyleSheet } from "react-native";
@@ -11,26 +11,36 @@ export function FeedInputDialog({ feedList, visible, setVisible, saveFeedFN}) {
   const [text, setText] = useState('');
   const [loading, setLoading] = useState(false);
   const hideDialog = () => { setVisible(false) };
-
+  const [error, setError] = useState('');
  
 
   const fetchURL = async () => {
     //should fetch from URL, for now just return placeholder text
+    
     setLoading(true);
-    const foundFeed = await fetchFeed(text);
-    if(foundFeed !== null){
-      foundFeed.rss.channel.feedLink = text;
-      console.log();
-      //add feed to list & save to cache
-      feedList.push(foundFeed.rss.channel);
-      await saveFeedFN();
-      hideDialog();
+    try{
+      const foundFeed = await fetchFeed(text);
+      if(foundFeed !== null){
+        foundFeed.rss.channel.feedLink = text;
+        console.log();
+        //add feed to list & save to cache
+        feedList.push(foundFeed.rss.channel);
+        await saveFeedFN();
+        hideDialog();
+      }
+      else
+      {
+        console.log('caught')
+        setError('Feed not found.')
+      }
+      setLoading(false);
     }
-    else
-    {
-      console.log('caught')
+    catch (error){
+      setError('Feed not found: ' + error)
+      setLoading(false);
     }
-    setLoading(false);
+    
+    
   }
 
 
@@ -42,7 +52,9 @@ export function FeedInputDialog({ feedList, visible, setVisible, saveFeedFN}) {
           <Dialog.Content>
           <TextInput label="RSS URL"  
           value={text}
-          onChangeText={text => setText(text)}></TextInput>
+          onChangeText={text => setText(text)}
+          error={(error.length > 0)}></TextInput>
+          {(error.length > 0) && <HelperText>Error: {error}</HelperText>}
         </Dialog.Content>
         <Dialog.Actions>
           <Button onPress={hideDialog}>Cancel</Button>
